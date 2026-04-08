ceedb6fde160153b57debed10c48d89add1e6ae6
Gustavo Carbonera <gustavo.carbonera@atplus.com.br>
2026-04-08 16:19:57 -0300

    feat: US-001 - Expandir configuração de ambiente para autenticação

--- src/_tests/mock-env.ts
+++ src/_tests/mock-env.ts
@@ -5,6 +5,6 @@ vi.mock("#/env", () => ({
 		SERVER_URL: "https://server.example.com",
 		CRM_NOCOBASE_URL: "https://example.com/api",
 		VITE_LOCAL_STORAGE_BASE_KEY: "custom-app",
-		CRM_NOCOBASE_TOKEN: "mock-token",
+		AUTH_SESSION_SECRET: "mock-auth-session-secret-at-least-32-chars",
 	},
 }));
