d5857984499dca2ed9102a96d996a56670706f7b
Gustavo Carbonera <gustavo.carbonera@atplus.com.br>
2026-04-28 13:55:37 -0300

    env fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { analyzer } from "vite-bundle-analyzer";
 
+const env = loadEnv(process.env.NODE_ENV ?? "development", process.cwd(), "");
+
 export default defineConfig({
-	plugins: [tailwindcss(), viteReact(), analyzer()],
+	plugins: [
+		tailwindcss(),
+		viteReact(),
+		analyzer({
+			analyzerMode: "static",
+		}),
+	],
 	resolve: {
 		tsconfigPaths: true,
 	},
 	server: {
-		allowedHosts: ["crm.gugacarbo.space"],
+		allowedHosts: [env.VITE_APP_URL],
 	},
 });
